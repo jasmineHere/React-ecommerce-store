f6070ccdbe638e683f8e1465a586fb8e407ed2e2
Jasmine Muman <jasminemuman21@gmail.com>
2023-06-29 07:58:43 -0400

    Context setup

---
+++ src/Context.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+// creates the context and initializes the context data to an empty object
+const Context = React.createContext({});
+
+export default Context;
---
+++ src/withContext.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Context from "./Context";
+
+// a component wrapper, which we’ll use to wrap components that use the context data and methods
+// HOC - Higher Order Component
+const withContext = WrappedComponent => {
+  const WithHOC = props => {
+    return (
+      <Context.Consumer>
+        {context => <WrappedComponent {...props} context={context} />}
+      </Context.Consumer>
+    );
+  };
+
+  return WithHOC;
+};
+
+export default withContext;
