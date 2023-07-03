9fa24bef5db7f6937f41240cc72be031bab7a331
Jasmine Muman <jasminemuman21@gmail.com>
2023-07-02 23:46:10 -0400

    cart management completed

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext } from "react";
 
 // creates the context and initializes the context data to an empty object
 const Context = React.createContext({});
--- src/pages/cartScreen.js
+++ src/pages/cartScreen.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useContext } from "react";
+import Context from "../Context";
+import CartItem from "../components/CartItem";
 
-const CartScreen = () => {
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Center',
-		alignItems: 'Center',
-		height: '100vh'
-	}}
-	>
-	<h1>This is the cart screen</h1>
-	</div>
-);
+const Cart = () => {
+  const { cart, removeFromCart, clearCart, checkout } = useContext(Context);
+  const cartKeys = Object.keys(cart || {});
+
+  return (
+    <>
+      <div className="hero is-primary">
+        <div className="hero-body container">
+          <h4 className="title">My Cart</h4>
+        </div>
+      </div>
+      <br />
+      <div className="container">
+        {cartKeys.length ? (
+          <div className="column columns is-multiline">
+            {cartKeys.map((key) => (
+              <CartItem
+                cartKey={key}
+                key={key}
+                cartItem={cart[key]}
+                removeFromCart={removeFromCart}
+              />
+            ))}
+            <div className="column is-12 is-clearfix">
+              <br />
+              <div className="is-pulled-right">
+                <button
+                  onClick={clearCart}
+                  className="button is-warning"
+                >
+                  Clear cart
+                </button>{" "}
+                <button
+                  className="button is-success"
+                  onClick={checkout}
+                >
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="column">
+            <div className="title has-text-grey-light">No item in cart!</div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
-export default CartScreen;
+export default Cart;
